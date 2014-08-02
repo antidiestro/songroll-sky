9326efa8e51a3ff890587a35069bac2cd9f25d41
Rene Morales <rene.morales.sanchez@gmail.com>
2014-08-02 17:14:47 -0400

    ugly login loop fix, more comments and logs

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -1,19 +1,28 @@
 Template.login.rendered = function(){
+	console.log('Login page rendered');
 	Template.login.redirect = Deps.autorun(function(){
 		nextPathDependency.depend();
-		if ( Meteor.userId() ) {
-			var next_path = nextPath;
-			if ( next_path ) {
-				nextPath = false;
-				Router.go(next_path);
-			} else {
-				Router.go('/');
+		if ( Meteor.user() ) {
+			console.log('User is NOW logged in.');
+			console.log('nextPath is '+nextPath);
+			if ( nextPath == false ) {
+				nextPath = '/';
 			}
+			console.log('Going to '+nextPath+' now.');
+			Router.go(nextPath);
+		} else {
+			console.log('User is not logged in yet. Waiting.')
 		}
 	});
 }
 
 
 Template.login.destroyed = function(){
+	console.log('Login page destroyed.');
+	// After our user leaves, stop monitoring for their login and nextPath.
 	Template.login.redirect.stop();
+
+	console.log('Setting nextPath to false.');
+	// And empty that stupid nextPath variable.
+	nextPath = false;
 }
