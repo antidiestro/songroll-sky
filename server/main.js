935003b13294b7b7c1b8df627666ece40ab75cf8
René A. Morales <rene.morales.sanchez@gmail.com>
2014-10-11 15:28:08 -0300

    Remove non-working Twitter api call
    Depends on the twitter-api meteor package which hasn’t been updated for
    0.9.x

--- server/main.js
+++ server/main.js
@@ -21,21 +21,6 @@ Accounts.onCreateUser(function(options, user){
 	return user;
 });
 
-Accounts.onLogin(function(e) {
-	var user = e.user;
-	var url = "https://api.twitter.com/1.1/users/show.json?screen_name="+user.username;
-	twitter.get(url, user.services.twitter.accessToken, user.services.twitter.accessTokenSecret, Meteor.bindEnvironment(function(err, d, r){
-		if ( !err ) {
-			var account = Meteor.users.findOne({_id: user._id});
-			if ( account ) {
-				var twitter_data = JSON.parse(d);
-				Meteor.users.update({_id: account._id}, { $set: { name: twitter_data.name, username: twitter_data.screen_name, avatar: twitter_data.profile_image_url_https } });
-			}
-		}
-	}));
-
-});
-
 // Kick users on exit
 UserStatus.events.on("connectionLogout", function(fields) {
 	var userCheck = Meteor.users.findOne({_id: fields.userId, 'status.online': true});
